9276d000bb9fe328164ad66e0bbcd8156bec8f02
Aline Pinho <alineveira2@gmail.com>
2025-05-08 19:53:36 -0300

    Delete prog2.js

--- aulajs02-20/prog2.js
+++
@@ -1,7 +0,0 @@
-/* Esturura de condicional */
-var nm = prompt('Digite o nome: ');
-if(nm==="maria") {
-    document.write("O nome digitado foi "+nm+" maria");
-} else {
-    document.write("O nome digitado foi "+nm);
-}
